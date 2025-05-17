a9d895d436a799797b1540fdd773150fb7535cb8
sarkiisov <sarkisov1337@gmail.com>
2025-05-17 12:43:26 +0300

    chore: remove type module

--- index.ts
+++ index.ts
@@ -2,29 +2,31 @@ import { getInput } from "@actions/core";
 import { context, getOctokit } from "@actions/github";
 import { Telegraf } from "telegraf";
 
-const telegramBotToken = getInput("telegram-bot-token");
-const telegramChatId = getInput("telegram-chat-id");
-const githubToken = getInput("github-token");
+(async () => {
+  const telegramBotToken = getInput("telegram-bot-token");
+  const telegramChatId = getInput("telegram-chat-id");
+  const githubToken = getInput("github-token");
 
-const bot = new Telegraf(telegramBotToken);
+  const bot = new Telegraf(telegramBotToken);
 
-// console.log(context);
+  // console.log(context);
 
-const octokit = getOctokit(githubToken);
+  const octokit = getOctokit(githubToken);
 
-const runId = context.runId;
-const { owner, repo } = context.repo;
+  const runId = context.runId;
+  const { owner, repo } = context.repo;
 
-const {
-  data: { jobs },
-} = await octokit.rest.actions.listJobsForWorkflowRun({
-  owner,
-  repo,
-  run_id: runId,
-});
+  const {
+    data: { jobs },
+  } = await octokit.rest.actions.listJobsForWorkflowRun({
+    owner,
+    repo,
+    run_id: runId,
+  });
 
-console.log("jobs", jobs);
+  console.log("jobs", jobs);
 
-telegramChatId.split(",").forEach((id) => {
-  bot.telegram.sendMessage(id, "Hello from Telegram bot");
-});
+  telegramChatId.split(",").forEach((id) => {
+    bot.telegram.sendMessage(id, "Hello from Telegram bot");
+  });
+})();
