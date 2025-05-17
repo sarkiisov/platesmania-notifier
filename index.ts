c21a7d5ba452246d0cd76c375f65359b80663905
sarkiisov <sarkisov1337@gmail.com>
2025-05-17 12:38:58 +0300

    feat: get ci data

--- index.ts
+++ index.ts
@@ -1,13 +1,29 @@
 import { getInput } from "@actions/core";
-import { context } from "@actions/github";
+import { context, getOctokit } from "@actions/github";
 import { Telegraf } from "telegraf";
 
 const telegramBotToken = getInput("telegram-bot-token");
 const telegramChatId = getInput("telegram-chat-id");
+const githubToken = getInput("github-token");
 
 const bot = new Telegraf(telegramBotToken);
 
-console.log(context);
+// console.log(context);
+
+const octokit = getOctokit(githubToken);
+
+const runId = context.runId;
+const { owner, repo } = context.repo;
+
+const {
+  data: { jobs },
+} = await octokit.rest.actions.listJobsForWorkflowRun({
+  owner,
+  repo,
+  run_id: runId,
+});
+
+console.log("jobs", jobs);
 
 telegramChatId.split(",").forEach((id) => {
   bot.telegram.sendMessage(id, "Hello from Telegram bot");
